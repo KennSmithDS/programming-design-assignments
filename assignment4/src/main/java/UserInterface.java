9c2a553bfa841655195b2bcdf4e78a837bf3247e
isidoraconic <conic.i@northeastern.edu>
2020-11-05 20:14:31 -0800

    Started making the UserInterface class but switching to a new feature branch for that now.

--- assignment4/src/main/java/UserInterface.java
+++ assignment4/src/main/java/UserInterface.java
@@ -1,6 +1,30 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserInterface {
 
+  private String directory;
+  private List<Grammar> grammarList;
+
+  public UserInterface() {
+    this.directory = null;
+    this.grammarList = new ArrayList<>();
+  }
+
+  public void setDirectory(String directory) throws NoSuchDirectoryException {
+    //Check if the output directory exists, if not, throw NoSuchDirectoryException
+    if (!(new File(directory).exists())) {
+      throw new NoSuchDirectoryException("The specified directory does not exist. "
+          + "Please enter an existing directory.");
+    } else {
+      this.directory = directory;
+    }
+  }
+
+  public static void main(String[] args) {
 
 
+  }
 
 }
