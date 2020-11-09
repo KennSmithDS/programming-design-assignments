1f135bd01c18e12ef0a3b355a61a34a078733a21
isidoraconic <conic.i@northeastern.edu>
2020-11-09 12:10:21 -0800

    I had started a UI tests branch lol.

---
+++ assignment4/src/test/java/UserInterfaceTest.java
@@ -0,0 +1,43 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserInterfaceTest {
+
+  @Test
+  public void setDirectory() {
+
+  }
+
+  @Test
+  public void getDirectory() {
+  }
+
+  @Test
+  public void addGrammar() {
+  }
+
+  @Test
+  public void menuCommand() {
+  }
+
+  @Test
+  public void handleInput() {
+  }
+
+  @Test
+  public void testToString() {
+  }
+
+  @Test
+  public void testEquals() {
+  }
+
+  @Test
+  public void testHashCode() {
+  }
+
+  @Test
+  public void main() {
+  }
+}
