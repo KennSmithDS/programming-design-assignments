ab2e38be45ab72262e542a0401dd4ab01b324766
Kendall Smith <smith.kendall.lee@gmail.com>
2020-11-19 11:57:31 -0800

    added javadoc comments

--- assignment5/src/main/java/CSVReader.java
+++ assignment5/src/main/java/CSVReader.java
@@ -3,16 +3,29 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.ParseException;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Objects;
+
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 
+/**
+ * Class to represent a CSV reader object encapsulating the commons-csv library
+ * Inputs a string path to the student clicks csv file, and then outputs a hashmap of click aggregations
+ * Downstream the CSV writer will take the hashmap and write unique keys to different files
+ */
 public class CSVReader {
 
     private final String[] STUDENT_HEADERS = {"module", "presentation", "student", "site", "date", "clicks"};
     private String csvFile;
 
+    /**
+     * Constructor method for CSVReader object
+     * @param csvFile String path to CSV file
+     * @throws FileNotFoundException default FileNotFoundException error
+     */
     public CSVReader(String csvFile) throws FileNotFoundException {
         if (!(new File(csvFile).exists())) {
             throw new FileNotFoundException("The specified CSV file does not exist in provided directory."
@@ -23,8 +36,13 @@ public class CSVReader {
     }
 
     /**
-     * Method that reads the CSV file and returns a data object containing the key lookup
-     * for clicks aggregated on module, presentation and date
+     * Method that reads the CSV file and returns a data object containing the key lookup for clicks aggregated on module, presentation and date
+     * The method creates the data hashmap object, and loops through the rows of the parsed CSV file
+     * For each row, it looks in the hashmap for module + presentation key
+     * If the key is found it looks in the nested hashmap for date key
+     * If the key is found, it adds the clicks together for new total
+     * Else it adds a new entry to the nested hashmap
+     * Lastly if the concat string key is not found, it adds an entirely new entry
      * @return HashMap with String key and HashMap of value with Integers as key and value for date and sum clicks
      * @throws IOException default IOException error
      * @throws IllegalArgumentException default IllegalArgumentException error
@@ -64,6 +82,13 @@ public class CSVReader {
         return aggStudentData;
     }
 
+    /**
+     * Main to test the functionality of the readCSVFile() method of CSVReader class
+     * Validates that the aggregations are occurring properly
+     * @param args String list of arguments
+     * @throws IOException default IOException error
+     * @throws IllegalArgumentException default IllegalArgumentException error
+     */
     public static void main(String[] args) throws IOException, IllegalArgumentException {
         String testFile = "anonymisedData/studentVle_sample.csv";
         CSVReader csvReader = new CSVReader(testFile);
@@ -75,4 +100,37 @@ public class CSVReader {
         }
     }
 
+    /**
+     * Override method for default equals()
+     * @param o class object for CSVReader
+     * @return boolean
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CSVReader csvReader = (CSVReader) o;
+        return Objects.equals(csvFile, csvReader.csvFile);
+    }
+
+    /**
+     * Override method for default hashCode()
+     * @return int
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(csvFile);
+    }
+
+    /**
+     * Override method for default toString()
+     * @return String
+     */
+    @Override
+    public String toString() {
+        return "CSVReader{" +
+                "STUDENT_HEADERS=" + Arrays.toString(STUDENT_HEADERS) +
+                ", csvFile='" + csvFile + '\'' +
+                '}';
+    }
 }
