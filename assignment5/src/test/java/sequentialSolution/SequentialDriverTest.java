afbcfb50ef0d33c81bab8d6891110735425b1480
Kendall Smith <smith.kendall.lee@gmail.com>
2020-11-29 17:13:55 -0800

    sequential driver test, jacoco at 90%

--- assignment5/src/test/java/sequentialSolution/SequentialDriverTest.java
+++ assignment5/src/test/java/sequentialSolution/SequentialDriverTest.java
@@ -3,15 +3,14 @@ package sequentialSolution;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
+
 import static org.junit.Assert.*;
 
 public class SequentialDriverTest {
 
-    @Before
-    public void setUp() throws Exception {
-    }
-
     @Test
-    public void main() {
+    public void main() throws NullCommandLineArgument, NoSuchDirectoryException, IOException {
+        SequentialDriver.main(new String[]{"anonymisedData"});
     }
 }
