474434cc9bb3ca81db8fabfa67fe8356d354b335
Kendall Smith <smith.kendall.lee@gmail.com>
2020-11-29 17:04:56 -0800

    finished tests for CSVRow

---
+++ assignment5/src/test/java/sequentialSolution/SequentialDriverTest.java
@@ -0,0 +1,17 @@
+package sequentialSolution;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SequentialDriverTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void main() {
+    }
+}
