50cb973bec02773e1360d93f2db76411eae5313d
Kendall Smith <smith.kendall.lee@gmail.com>
2020-12-09 17:53:14 -0800

    pushing simple client/server applet

---
+++ assignment6/src/main/java/Server.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+//    private ObjectInputStream messageInput;
+//    private ObjectOutputStream messageOutput;
+    private DataInputStream dataIn;
+    private DataOutputStream dataOut;
+    private static final String DEFAULT_HOST = "127.0.0.1";
+    private static final int DEFAULT_PORT = 3333;
+
+    public static void main(String[] args) throws IOException {
+        new Server();
+    }
+
+    /**
+     * constructor for the Server class, which will take
+     * @throws IOException
+     */
+    public Server() throws IOException {
+        try {
+            this.serverSocket = new ServerSocket(DEFAULT_PORT);
+            this.clientSocket = serverSocket.accept();
+            this.dataIn = new DataInputStream(clientSocket.getInputStream());
+            this.dataOut = new DataOutputStream(clientSocket.getOutputStream());
+//            this.messageInput = new ObjectInputStream(clientSocket.getInputStream());
+//            this.messageOutput = new ObjectOutputStream(clientSocket.getOutputStream());
+            listenForClient();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    // this is what will become multithreaded
+    public void listenForClient() {
+        while (true) {
+            try {
+                while (dataIn.available() == 0)
+//                while (messageInput.available() == 0) {
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                String input = dataIn.readUTF();
+                dataOut.writeUTF(input);
+//                String inboundMessage = messageInput.readUTF();
+//                messageOutput.writeUTF(inboundMessage);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+
+        try {
+//            messageInput.close();
+//            messageOutput.close();
+            this.clientSocket.close();
+            this.serverSocket.close();
+        } catch (IOException e) {
+                e.printStackTrace();
+        }
+    }
+
+}
---
+++ assignment6/src/main/java/SimpleClient.java
@@ -0,0 +1,69 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class SimpleClient {
+
+    private Socket socket;
+    private DataInputStream dataIn;
+    private DataOutputStream dataOut;
+    
+    public static void main(String[] args) throws IOException {
+        new SimpleClient();
+    }
+
+    public SimpleClient() throws IOException {
+        try {
+            socket = new Socket("localhost", 3333); // don't use a port below 1024
+            dataIn = new DataInputStream(socket.getInputStream());
+            dataOut = new DataOutputStream(socket.getOutputStream());
+            listenForServer();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void listenForServer() throws IOException {
+        Scanner console = new Scanner(System.in);
+        while (true) {
+            while (!console.hasNextLine()) {
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            String input = console.nextLine();
+            if (input.toLowerCase().equals("@quit")) {
+                break;
+            }
+            try {
+                dataOut.writeUTF(input);
+
+                while (dataIn.available() == 0) {
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                String reply = dataIn.readUTF();
+                System.out.println(reply);
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+        try {
+            dataIn.close();
+            dataOut.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
