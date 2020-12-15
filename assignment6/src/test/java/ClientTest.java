d1b98cf860c26c49933ed3ed52506b970f473a85
isidoraconic <conic.i@northeastern.edu>
2020-12-15 12:14:46 -0800

    Made Client Test class

---
+++ assignment6/src/test/java/ClientTest.java
@@ -0,0 +1,31 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ClientTest {
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @Test
+  public void getClientSocket() {
+  }
+
+  @Test
+  public void listenForUserCommands() {
+  }
+
+  @Test
+  public void testEquals() {
+  }
+
+  @Test
+  public void testHashCode() {
+  }
+
+  @Test
+  public void testToString() {
+  }
+}
